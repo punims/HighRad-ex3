741e3a4b076ca723a717fe4e68e1e2d98fbc0e6d
punims <edanpatt@gmail.com>
2023-03-09 08:01:13 +0200

    Fixed bug, I thought min val was 500 but it should have been -500. I also added denoising and found the largest connected component

--- HardROI.py
+++ HardROI.py
@@ -1,7 +1,10 @@
+import os
+
 import numpy as np
+from NiftyHandler import NiftyHandler
+from scipy.ndimage import label, binary_erosion, binary_closing, binary_dilation, binary_opening, generate_binary_structure
 
-
-class DifficultSkeletonSegmentation:
+class DifficultBodySegmentation:
     """
     Class responsible for a more difficult segmentation of parts of the skeleton
     """
@@ -17,13 +20,11 @@ class DifficultSkeletonSegmentation:
         """
 
         # First we threshold
-        MIN_VAL = 500
+        MIN_VAL = -500
         MAX_VAL = 2000
-        thresholded_ct = ((ct_scan >= MIN_VAL) & (ct_scan <= MAX_VAL)).astype(int)
-
+        thresholded_ct = ((ct_scan >= MIN_VAL) & (ct_scan <= MAX_VAL)).astype(float)
         denoised_ct = self.__denoise_body(thresholded_ct)
         body_segmentation = self.__compute_largest_connected_component(denoised_ct)
-
         return body_segmentation
 
     def isolate_bs(self):
@@ -32,9 +33,60 @@ class DifficultSkeletonSegmentation:
     def three_d_band(self):
         pass
 
-    def __denoise_body(self, thresholded_ct):
-        pass
+    def __denoise_body(self, thresholded_ct: np.ndarray) -> np.ndarray:
+        """
+        Given a thresholded scan of the body, denoise to get rid of
+        artifacts.
+        @param thresholded_ct:
+        @return: cleaned version of the body
+        """
+
+        rank = 3
+        connectivity = 2
+        structuring_element = generate_binary_structure(rank, connectivity)
+        x = thresholded_ct
+        x = binary_opening(x, structuring_element, iterations=2)
+        x = binary_closing(x, structuring_element)
+        return x
+
 
     def __compute_largest_connected_component(self, denoised_ct):
-        pass
+        """
+        given denoised ct return segmentation of the largest connected compoennt
+        which is likely to be the body
+        @param denoised_ct:
+        @return:
+        """
 
+        x = denoised_ct
+        x, num_components = label(x)
+        x = x == np.argmax(np.bincount(x.flat)[1:]) + 1
+        x = x.astype(float)
+        return x
+
+def get_skeleton_segmentation(ct_scan_path):
+    """
+    global entry function for part 1 of the exercise
+    @param ct_scan_path:
+    @return:
+    """
+
+    output_dir = "skeleton_segmentation_output"
+    scan_basename = os.path.basename(ct_scan_path).split('.')[0]
+    if not os.path.isdir(output_dir):
+        os.mkdir(output_dir)
+    data, orientation = NiftyHandler.read(ct_scan_path)
+    dss = DifficultBodySegmentation()
+    body_segmentation = dss.isolate_body(data)
+    body_segmentation_path = os.path.join(output_dir, f"{scan_basename}_body_segmentation.nii.gz")
+    NiftyHandler.write(body_segmentation, body_segmentation_path, orientation)
+
+
+def main():
+
+    ct_scan_path = "/home/edan/Desktop/HighRad/Exercises/data/Targil1_data-20230227T131201Z-001/Targil1_data/Case1_CT.nii.gz"
+    get_skeleton_segmentation(ct_scan_path)
+
+
+if __name__ == '__main__':
+    main()
---
+++ NiftyHandler.py
@@ -0,0 +1,54 @@
+import os
+import nibabel as nib
+from nibabel.orientations import io_orientation, axcodes2ornt, ornt_transform
+import numpy as np
+class NiftyHandler:
+    """
+    Class responsible for the reading and writing of nifti files.
+    """
+
+    @classmethod
+    def read(cls, nifti_file_path: str):
+        """
+        reads the nifti file that is in a gz format.
+        :param nifti_file_path:
+        :return: data and orientation data
+        """
+
+        nifti_file_path = cls.correct_filename(nifti_file_path)
+        if not os.path.exists(nifti_file_path):
+            raise ValueError(f"File name {nifti_file_path} does not exist")
+        nifti_file = nib.load(nifti_file_path)  # might raise an exception
+
+        original_orientation = io_orientation(nifti_file.affine)
+        ras_ornt = axcodes2ornt("RAS")
+
+        to_canonical = original_orientation  # Same as ornt_transform(img_ornt, ras_ornt)
+        from_canonical = ornt_transform(ras_ornt, original_orientation)
+
+        # Same as as_closest_canonical
+        img_canonical = nifti_file.as_reoriented(to_canonical)
+        img_data = img_canonical.get_fdata()
+        return img_data, from_canonical
+
+    @classmethod
+    def write(cls, data, file_name: str, orientation=None):
+        """
+        :param data: ndarray of the data
+        :param nifti_file: nifti file that was opened
+        :param file_name: file name for saving
+        :return:
+        """
+
+        file_name = cls.correct_filename(file_name)
+        new_nifti = nib.Nifti1Image(data, np.eye(4))
+        new_nifti = new_nifti.as_reoriented(orientation)
+        nib.save(new_nifti, file_name)
+
+    @staticmethod
+    def correct_filename(file_name):
+        extension = ".nii.gz"
+        if os.path.basename(file_name).endswith(extension):
+            return file_name
+        else:
+            return file_name + extension
